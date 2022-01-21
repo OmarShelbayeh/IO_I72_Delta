828d6ae5dac84c4c977edb210cc00795e136b9ad
rtzw1 <rtzw11@gmail.com>
2022-01-21 14:13:52 +0100

    Update

--- src/main/java/com/example/IO/service/CompareJSON.java
+++ src/main/java/com/example/IO/service/CompareJSON.java
@@ -21,14 +21,14 @@ public class CompareJSON extends Decorator {
     public ArrayList<Integer> compareJSON(String firstJSON, String secondJSON) {
         String[] firstJSONArray = firstJSON.split("\n");
         String[] secondJSONArray = secondJSON.split("\n");
-        int length = firstJSON.length() > secondJSONArray.length ? secondJSONArray.length : firstJSONArray.length;
+        int length = Math.min(firstJSONArray.length, secondJSONArray.length);
         ArrayList<Integer> returnArrayList = new ArrayList<>();
         for (int i = 0; i < length; i++) {
             if (!firstJSONArray[i].equals(secondJSONArray[i])) returnArrayList.add(i + 1);
         }
-        int temp = firstJSON.length() < secondJSONArray.length ? secondJSONArray.length : firstJSONArray.length - length;
-        for (int i = length; i < temp + length; i++) {
-            returnArrayList.add(length + 1);
+        int temp = Math.max(firstJSONArray.length, secondJSONArray.length);
+        for (int i = length; i < temp; i++) {
+            returnArrayList.add(i + 1);
         }
         return returnArrayList;
     }
