599480ee5c4282cd61e392b8b1ff4293558f4fcc
rtzw1 <rtzw11@gmail.com>
2022-01-22 20:23:48 +0100

    Update

--- src/test/java/com/example/IO/service/TransformJSONSpecificPropertiesTest.java
+++ src/test/java/com/example/IO/service/TransformJSONSpecificPropertiesTest.java
@@ -51,7 +51,7 @@ class TransformJSONSpecificPropertiesTest {
                         "{\"quiz\":{\"sport\":{\"q1\":{\"question\":\"Which one is correct team name in NBA?\",\"options\":[\"New York Bulls\",\"Los Angeles Kings\",\"Golden State Warriros\",\"Huston Rocket\"],\"answer\":\"Huston Rocket\"}},\"maths\":{\"q1\":{\"question\":\"5 + 7 = ?\",\"options\":[\"10\",\"11\",\"12\",\"13\"],\"answer\":\"12\"},\"q2\":{\"question\":\"12 - 8 = ?\",\"options\":[\"1\",\"2\",\"3\",\"4\"],\"answer\":\"4\"}}}}",
                         specificProperties);
         assertEquals(
-                "{\"quiz\":{\"sport\":{\"q1\":{\"options\":[\"New York Bulls\",\"Los Angeles Kings\",\"Golden State Warriros\",\"Huston Rocket\"],},\"maths\":{\"q1\":{\"options\":[\"10\",\"11\",\"12\",\"13\"],\"q2\":{\"options\":[\"1\",\"2\",\"3\",\"4\"],}}}",
+                "{\"quiz\":{\"sport\":{\"q1\":{\"options\":[\"New York Bulls\",\"Los Angeles Kings\",\"Golden State Warriros\",\"Huston Rocket\"] }},\"maths\":{\"q1\":{\"options\":[\"10\",\"11\",\"12\",\"13\"] },\"q2\":{\"options\":[\"1\",\"2\",\"3\",\"4\"] }}}}",
                 transformJSONSpecificProperties.transformJSONSpecificProperties(compareClass));
 
     }
